a0b6b44af36d87ee92c598f5f5d96740e278dde7
Antoine Vandermeersch <avdmeers@gmail.com>
2020-07-17 21:52:47 +0200

    add python3.8 to path in cron

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@ influx <<EOF
 CREATE DATABASE grandexchange
 EOF
 
+echo "PATH=/bin:/usr/bin:/usr/local/bin" >> mycron
 echo "0 */12 * * * cd /home/grandexchange && ./poll_grandexchange.sh" >> mycron
 crontab mycron
 rm mycron
