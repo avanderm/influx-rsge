a5724daafd3a6e8675311c50cc0a3a2723ee4d5c
Antoine Vandermeersch <avdmeers@gmail.com>
2020-07-17 18:03:02 +0200

    python 3.8

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo yum update -y
+
+# Using RHEL 6 instead of latest
 cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
 [influxdb]
 name = InfluxDB Repository - RHEL 6
@@ -15,4 +18,11 @@ sudo yum localinstall chronograf-1.8.5.x86_64.rpm -y
 wget https://dl.influxdata.com/kapacitor/releases/kapacitor-1.5.5-1.x86_64.rpm
 sudo yum localinstall kapacitor-1.5.5-1.x86_64.rpm -y
 
-sudo yum install jq python3 -y
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel jq -y
+cd /opt
+sudo wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
+sudo tar xzf Python-3.8.2.tgz
+cd Python-3.8.2
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo rm -f /opt/Python-3.8.2.tgz
