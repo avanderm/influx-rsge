3e5903d194eb558a368144abe636a53a3cb226d0
Antoine Vandermeersch <avdmeers@gmail.com>
2020-07-17 17:37:21 +0200

    approve yum installs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,11 +8,11 @@ gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
 
-sudo yum install telegraf
-sudo yum install influxdb
+sudo yum install telegraf -y
+sudo yum install influxdb -y
 wget https://dl.influxdata.com/chronograf/releases/chronograf-1.8.5.x86_64.rpm
-sudo yum localinstall chronograf-1.8.5.x86_64.rpm
+sudo yum localinstall chronograf-1.8.5.x86_64.rpm -y
 wget https://dl.influxdata.com/kapacitor/releases/kapacitor-1.5.5-1.x86_64.rpm
-sudo yum localinstall kapacitor-1.5.5-1.x86_64.rpm
+sudo yum localinstall kapacitor-1.5.5-1.x86_64.rpm -y
 
-sudo yum install jq
+sudo yum install jq -y
