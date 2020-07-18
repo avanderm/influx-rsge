91bbc29f1104abcf53591de263b5ad6d3a75b65d
Antoine Vandermeersch <avdmeers@gmail.com>
2020-07-18 23:52:56 +0200

    poll at midnight

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,6 @@ CREATE DATABASE grandexchange
 EOF
 
 echo "PATH=/bin:/usr/bin:/usr/local/bin" >> mycron
-echo "0 */12 * * * cd /home/grandexchange && ./poll_grandexchange.sh" >> mycron
+echo "0 0 * * * cd /home/grandexchange && ./poll_grandexchange.sh" >> mycron
 crontab mycron
 rm mycron
